cd2b0d016383f1eb15bd38e61859de8690827326
AndyMelone <meloneandy225@gmail.com>
2024-11-17 19:23:26 +0000

    ui: add ui for filter for skill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AddDjobsForm from "@/components/shared/addDjobsForm";
 import WorkItem from "@/components/shared/workItem";
+import { IoMdClose } from "react-icons/io";
 
 export default function Home() {
   return (
@@ -7,8 +8,25 @@ export default function Home() {
       <div className="min-h-screen flex flex-col">
         <div className="relative h-24 sm:h-28 md:h-32   w-full bg-[hsl(180,29%,50%)]  bg-header-mobile md:bg-header-desktop flex justify-center items-center ">
           <div
-            className={`absolute bottom-0  h-16 w-4/5 bg-[hsl(180,52%,96%)]  translate-y-8 mx-auto rounded-lg shadow-md`}
-          ></div>
+            className={`flex items-center justify-between px-4  absolute bottom-0  h-16 w-4/5 bg-[hsl(180,52%,96%)]  translate-y-8 mx-auto rounded-lg shadow-md`}
+          >
+            <div className="flex flex-grow overflow-scroll  items-center gap-3">
+              <button className="group flex items-center justify-center space-x-2 bg-[hsl(180,29%,50%)] bg-opacity-20 rounded-lg">
+                <p className="text-[hsl(180,29%,50%)] font-bold px-2 py-1 ">
+                  Frontend
+                </p>
+                <div className="bg-[hsl(180,29%,50%)] p-2 rounded-r-lg group-hover:bg-black">
+                  <IoMdClose color="white" />
+                </div>
+              </button>
+            </div>
+
+            <button className="group ml-5">
+              <p className="text-gray-500 font-semibold group-hover:underline group-hover:text-[hsl(180,29%,50%)] group-active:opacity-70">
+                Effacer
+              </p>
+            </button>
+          </div>
         </div>
         <div className="bg-[hsl(180,52%,96%)] flex-grow flex-1 overflow-y-auto ">
           <div className="my-12 ">
